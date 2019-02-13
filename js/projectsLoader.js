5a5237f802315f1709ea6dc6ba6476fe82d9742b
Aviv Biton <avivbiton01@gmail.com>
2019-02-13 22:18:34 +0200

    tiny refactor of projectsLoader

--- js/projectsLoader.js
+++ js/projectsLoader.js
@@ -1,3 +1,74 @@
-$(document).ready(function() {
-	$.getJSON("../projectsData.json", function(data, textStatus, jqXHR) {});
+$(document).ready(function () {
+	let projectsHtml = "";
+	$.getJSON("../projectsData.json", function (data, textStatus, jqXHR) {
+		$.each(data, function (index, project) {
+			projectsHtml += parseHtmlForProject(project);
+		});
+
+		$("#projectsList").html(projectsHtml);
+	});
 });
+
+
+function parseHtmlForProject(project) {
+	let badges = "";
+	project.badges.forEach(b => {
+		badges += `<span class="badge badge-info ml-1">${b}</span>`;
+	});
+	return `			
+	<div class="row pb-4">
+		<div class="col">
+			<div class="card">
+				<h4 class="card-header text-center bg-info text-white">
+				${project.name}
+				</h4>
+				<div class="card-body row">
+					<div class="col-md-8">
+						<div class="pb-2">
+						   ${badges}
+						</div>
+						<p class="card-text">
+							${project.text}
+						</p>
+						 <a href="${project.siteUrl}" target="_blank" class="btn btn-primary btn-lg btn-block">View Site</a>
+						<a href="${project.sourceUrl}" target="_blank" class="btn btn-info btn-lg btn-block">Source Code</a>
+					</div>
+					<div class="col-md-4 d-none d-md-block">
+						<img src="img/backend.png" class="img-fluid">
+					</div>
+				</div>
+			</div>
+		</div>			
+	 </div>`;
+}
+
+/*
+			<div class="row pb-4">
+				<div class="col">
+					<div class="card">
+						<h4 class="card-header text-center bg-info text-white">Family Overseer</h4>
+						<div class="card-body row">
+							<div class="col-md-8">
+								<div class="pb-2">
+									<span class="badge badge-info">Fullstack</span>
+									<span class="badge badge-info">MongoDb</span>
+									<span class="badge badge-info">MERN</span>
+							   	</div>
+								<p class="card-text">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus totam voluptatibus
+									expedita ab unde culpa sapiente, distinctio odit explicabo animi mollitia amet
+									reiciendis. Cum expedita nam alias. Quam totam in fugit recusandae perferendis officia
+									nulla. Praesentium aliquam dicta ipsa impedit facere sed fugit rerum iure.
+								</p>
+								<button class="btn btn-primary btn-lg btn-block">View Site</button>
+								<button class="btn btn-info btn-lg btn-block">Source Code</button>
+							</div>
+							<div class="col-md-4 d-none d-md-block">
+								<img src="img/backend.png" class="img-fluid">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+                    */
