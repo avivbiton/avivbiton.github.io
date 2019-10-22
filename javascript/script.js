e941f9495d7e74eac8dadc50a82df514bc2dcc54
Aviv Biton <avivbiton01@gmail.com>
2019-10-22 12:32:26 +0300

    Modal appear when clicking a card

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,14 @@
-function onCardClick() {
-    console.log('clicked');
-}
+
+$("#cardModal").on('show.bs.modal', function (event) {
+    const button = $(event.relatedTarget) // Button that triggered the modal
+    const name = button.data("name");
+    const img = button.data("img");
+    const description = button.data("description");
+    const source = button.data("source_url");
+    const website = button.data("website_url"); // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    const modal = $(this)
+    //modal.find('.modal-title').text(name);
+    //modal.find('.modal-body').text(description);
+})
